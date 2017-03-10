24df288bc15ccb151d0ac0c890e96aa85ee5df1b
Lukas Stribrny <LukasStribrny@users.noreply.github.com>
2017-03-10 19:50:06 +0000

    Create Library.php

---
+++ Library.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class CI_Library {
+    
+    public function __construct(){
+        $CI =& get_instance();
+        foreach($CI AS $Key=>$Val){
+            $this->{$Key} = $Val;
+        }
+    }
+}
+?>
