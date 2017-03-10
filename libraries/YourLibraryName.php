13dc9338ec1bc1aca9d30de5c0a281a631f6d699
Lukas Stribrny <LukasStribrny@users.noreply.github.com>
2017-03-10 21:34:08 +0000

    Update YourLibraryName.php

--- libraries/YourLibraryName.php
+++ libraries/YourLibraryName.php
@@ -4,11 +4,12 @@ class YourLibraryName extends CI_Library {
 	
 	public function __construct(){
 		parent::__construct();
+		$this->Fun_Name();
 	}
 	
 	public function Fun_Name(){
 		//Do something or anything
+		echo __CLASS__ .'::' . __FUNCTION__ .'->'." It's working!";
 	}
-	
 }
 ?>
