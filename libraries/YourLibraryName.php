aea4e2796aa4efbe1e842d29910f2965a4c05350
Lukas Stribrny <LukasStribrny@users.noreply.github.com>
2017-03-10 21:15:13 +0000

    Create YourLibraryName.php

---
+++ libraries/YourLibraryName.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class YourLibraryName extends CI_Library {
+	
+	public function __construct(){
+		parent::__construct();
+	}
+	
+	public function Fun_Name(){
+		//Do something or anything
+	}
+	
+}
+?>
